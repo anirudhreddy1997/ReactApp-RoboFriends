7f71666ec1a98d6891bf72ea030cc06cee953908
Anirudh Reddy <anirudhreddykar@gmail.com>
2019-03-15 02:31:31 +0530

    Updates

---
+++ precache-manifest.ce1cd83f20496e30004a5d5ea72184c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9ee5031045dd65aec7",
+    "url": "/ReactApp-RoboFriends/static/js/runtime~main.8f913fea.js"
+  },
+  {
+    "revision": "fda87842e399fbfcb8bb",
+    "url": "/ReactApp-RoboFriends/static/js/main.ec7188da.chunk.js"
+  },
+  {
+    "revision": "517d0f327b2004615235",
+    "url": "/ReactApp-RoboFriends/static/js/2.e072e589.chunk.js"
+  },
+  {
+    "revision": "fda87842e399fbfcb8bb",
+    "url": "/ReactApp-RoboFriends/static/css/main.b01fb3b8.chunk.css"
+  },
+  {
+    "revision": "517d0f327b2004615235",
+    "url": "/ReactApp-RoboFriends/static/css/2.af0744b7.chunk.css"
+  },
+  {
+    "revision": "15a5884e9b884f0967d081bc5bb87c99",
+    "url": "/ReactApp-RoboFriends/index.html"
+  }
+];
