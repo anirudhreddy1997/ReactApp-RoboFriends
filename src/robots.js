b85ff8ed03b9fb38dd1c6232dfce847fc7e18ebd
Anirudh Reddy <anirudhreddykar@gmail.com>
2019-03-15 02:09:54 +0530

    First version

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots=[
+{
+	id:1,
+	name:'leela krishna',
+	username:'leela',
+	email:'leela@gmail.com'
+},
+{
+	id:2,
+	name:'sujith srinivas',
+	username:'sujith',
+	email:'sujith@gmail.com'
+},
+{
+	id:3,
+	name:'akhil kumar',
+	username:'akhil',
+	email:'akhil@gmail.com'
+},
+{
+	id:4,
+	name:'leela krishna1',
+	username:'leela',
+	email:'leela@gmail.com'
+},
+{
+	id:5,
+	name:'sujith srinivas1',
+	username:'sujith',
+	email:'sujith@gmail.com'
+},
+{
+	id:6,
+	name:'akhil kumar1',
+	username:'akhil',
+	email:'akhil@gmail.com'
+},
+{
+	id:7,
+	name:'leela krishna12',
+	username:'leela',
+	email:'leela@gmail.com'
+},
+{
+	id:8,
+	name:'sujith srinivas12',
+	username:'sujith',
+	email:'sujith@gmail.com'
+},
+{
+	id:9,
+	name:'akhil kumar12',
+	username:'akhil',
+	email:'akhil@gmail.com'
+}
+]
